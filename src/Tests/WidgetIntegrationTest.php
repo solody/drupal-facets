e2d476784c1c5153202ac915138ead5df3062b25
Leksat <Leksat@390336.no-reply.drupal.org>
2016-03-04 13:48:40 +0100

    Issue #2636328 by borisson_, Leksat: Widget dropdown - select

--- src/Tests/WidgetIntegrationTest.php
+++ src/Tests/WidgetIntegrationTest.php
@@ -7,6 +7,7 @@
 
 namespace Drupal\facets\Tests;
 
+use Drupal\Core\Url;
 use \Drupal\facets\Tests\WebTestBase as FacetWebTestBase;
 
 /**
@@ -190,6 +191,49 @@ class WidgetIntegrationTest extends FacetWebTestBase {
 
     $this->clickLink('item');
     $this->assertLink('(-) item');
+  }
+
+  /**
+   * Tests select widget's basic functionality.
+   */
+  public function testSelectWidget() {
+    $id = 'select_widget';
+    $name = 'Select';
+    $facet_add_page = 'admin/config/search/facets/add-facet';
+
+    $this->drupalGet($facet_add_page);
+
+    $form_values = [
+      'id' => $id,
+      'status' => 1,
+      'url_alias' => $id,
+      'name' => $name,
+      'facet_source_id' => 'search_api_views:search_api_test_view:page_1',
+      'facet_source_configs[search_api_views:search_api_test_view:page_1][field_identifier]' => 'type',
+    ];
+    $this->drupalPostForm(NULL, ['facet_source_id' => 'search_api_views:search_api_test_view:page_1'], $this->t('Configure facet source'));
+    $this->drupalPostForm(NULL, $form_values, $this->t('Save'));
+    $this->drupalPostForm(NULL, ['widget' => 'select'], $this->t('Save'));
+
+    $block_values = [
+      'plugin_id' => 'facet_block:' . $id,
+      'settings' => [
+        'region' => 'footer',
+        'id' => str_replace('_', '-', $id),
+      ],
+    ];
+    $this->drupalPlaceBlock($block_values['plugin_id'], $block_values['settings']);
+
+    $this->drupalGet('search-api-test-fulltext');
+    $this->assertField('edit-type', 'Dropdown is visible.');
+    $this->assertText('Displaying 5 search results');
+
+    $url = Url::fromUserInput('/search-api-test-fulltext', ['query' => ['f[0]' => 'select_widget:item']]);
+    $url->setAbsolute();
+
+    $this->drupalPostForm(NULL, ['type' => $url->toString()], $this->t('submit'));
+    $this->assertResponse(200);
+    $this->assertText('Displaying 3 search results');
   }
 
   /**
