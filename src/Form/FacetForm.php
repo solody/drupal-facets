ec781865606dcefa4b94b93a1003e5f300438cb1
git <git@2946661.no-reply.drupal.org>
2017-04-03 14:06:53 +0200

    Issue #2866167 by borisson_, alan-ps: Fix test failure

--- src/Form/FacetForm.php
+++ src/Form/FacetForm.php
@@ -578,7 +578,7 @@ class FacetForm extends EntityForm {
     // Only widgets that return an array can work with rest facet sources, so if
     // the user has selected another widget, we should point them to their
     // misconfiguration.
-    if ($facet->getFacetSource()->getDisplay() instanceof \Drupal\search_api\Plugin\search_api\display\ViewsRestDisplay) {
+    if ($facet->getFacetSource()->getDisplay() instanceof \Drupal\search_api\Plugin\search_api\display\ViewsRest) {
       if (strpos($values['widget'], 'array') === FALSE) {
         $form_state->setErrorByName('widget', $this->t('The Facet source is a Rest export. Please select a raw widget.'));
       }
